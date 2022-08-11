46c5b76ee8eecf0f3ac9559f37e864c13cb4d922
LaiTkiLLa <84670285+LaiTkiLLa@users.noreply.github.com>
2022-08-11 15:23:07 +0300

    Update test_courses_api.py

--- tests/students/test_courses_api.py
+++ tests/students/test_courses_api.py
@@ -14,13 +14,14 @@ def course_factory():
     def factory(*args, **kwargs):
         return baker.make(Course, *args, **kwargs)
     return factory
-#
+
 @pytest.fixture
 def student_factory():
     def factory(*args, **kwargs):
         return baker.make(Student, *args, **kwargs)
     return factory
 
+###проверка получения 1го курса
 @pytest.mark.django_db
 def test_oneCourse(client, course_factory):
 
@@ -32,7 +33,7 @@ def test_oneCourse(client, course_factory):
     assert response.json()['id'] == 4
 
 
-
+#### проверка получения списка курсов
 @pytest.mark.django_db
 def test_listCourse(client, course_factory):
 
@@ -42,6 +43,7 @@ def test_listCourse(client, course_factory):
 
     assert len(response.json()) == 5
 
+###тест успешного создания курса
 @pytest.mark.django_db
 def test_createCourse(client):
 
@@ -52,6 +54,7 @@ def test_createCourse(client):
     assert response.status_code == 201
     assert Course.objects.count() == base + 1
 
+###тест успешного обновления курса
 @pytest.mark.django_db
 def test_patchCourse(client):
 
@@ -62,7 +65,7 @@ def test_patchCourse(client):
 
     assert response_patch.status_code == 200
 
-
+###тест успешного удаления курса
 @pytest.mark.django_db
 def test_deleteCourse(client):
 
@@ -77,7 +80,7 @@ def test_deleteCourse(client):
     assert response_delete.status_code == 204
     assert Course.objects.count() == base
 
-
+###проверка фильтрации списка курсов по id
 @pytest.mark.django_db
 def test_filterIdCourse(client, course_factory):
 
@@ -87,6 +90,7 @@ def test_filterIdCourse(client, course_factory):
 
     assert response.status_code == 200
 
+###проверка фильтрации списка курсов по name
 @pytest.mark.django_db
 def test_filterNameCourse(client, course_factory):
 
@@ -96,6 +100,7 @@ def test_filterNameCourse(client, course_factory):
 
     assert response.status_code == 200
 
+###Ограничить число студентов на курсе
 @pytest.mark.django_db
 def test_StudentsNumber(client, course_factory):
 
